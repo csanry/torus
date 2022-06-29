cc6986ac0c6c9b97eba08839c1663016e17eb191
csanry <clarencesan@gmail.com>
2022-06-29 11:46:11 +0800

    refactor variables and naming conventions

--- pipelines/train_pipeline.py
+++ pipelines/train_pipeline.py
@@ -7,39 +7,50 @@ from kfp.v2.dsl import Artifact, Dataset, Input, Output, OutputPath, component
 
 import kfp
 
-# from sqlalchemy import outparam
+PROJECT_ID = "pacific-torus-347809"
+ML_PIPELINE_NAME = "ccd-train-pipeline" 
+BUCKET_NAME = "mle-dwh-torus"
+PIPELINE_ROOT = f"gs://{BUCKET_NAME}/pipeline/"
+REGION = "us-central1"
+URL_ROOT = "https://raw.githubusercontent.com/csanry/torus/main/pipelines/kfp_components"
+
+aip.init(project=PROJECT_ID, staging_bucket=BUCKET_NAME)
+
+# ----------------------------
+# Load in necessary components 
+# ----------------------------
+# ingest components
+ingest_op = kfp.components.load_component_from_url(f"{URL_ROOT}/ingest/bq_extract_data_component.yaml")
+
+# preprocessing components 
+basic_preprocessing_op = kfp.components.load_component_from_url(f"{URL_ROOT}/preprocessing/basic_preprocessing_component.yaml")
+tfdv_op = kfp.components.load_component_from_url(f"{URL_ROOT}/preprocessing/tfdv_generate_statistics_component.yaml")
+tfdv_drift_op = kfp.components.load_component_from_url(f"{URL_ROOT}/preprocessing/tfdv_detect_drift_component.yaml")
+
+# training components 
+train_test_split_data_op = kfp.components.load_component_from_url(f"{URL_ROOT}/training/train_test_split_data_component.yaml")
+train_tune_op = kfp.components.load_component_from_url(f"{URL_ROOT}/training/train_hptune.yaml")
+model_evaluation_op = kfp.components.load_component_from_url(f"{URL_ROOT}/training/model_evaluation.yaml")
+
+# deploy components 
+deploy_op = kfp.components.load_component_from_url(f"{URL_ROOT}/prediction/predict.yaml")
 
 
-PIPELINE_ROOT = "{}/pipeline/".format("gs://mle-dwh-torus")
-
-aip.init(project="pacific-torus-347809", staging_bucket="gs://mle-dwh-torus")
-
-ingest_op = kfp.components.load_component_from_url("https://raw.githubusercontent.com/csanry/torus/main/pipelines/kfp_components/ingest/bq_extract_data_component.yaml")
-tfdv_op = kfp.components.load_component_from_url("https://raw.githubusercontent.com/csanry/torus/main/pipelines/kfp_components/preprocessing/tfdv_generate_statistics_component.yaml")
-
-tfdv_drift_op = kfp.components.load_component_from_url("https://raw.githubusercontent.com/csanry/torus/main/pipelines/kfp_components/preprocessing/tfdv_detect_drift_component.yaml")
-
-basic_preprocessing_op = kfp.components.load_component_from_url("https://raw.githubusercontent.com/csanry/torus/main/pipelines/kfp_components/preprocessing/basic_preprocessing_component.yaml")
-train_test_split_data_op = kfp.components.load_component_from_url("https://raw.githubusercontent.com/csanry/torus/main/pipelines/kfp_components/training/train_test_split_data_component.yaml")
-
-train_tune_op = kfp.components.load_component_from_url("https://raw.githubusercontent.com/csanry/torus/main/pipelines/kfp_components/training/train_hptune.yaml")
-model_evaluation_op = kfp.components.load_component_from_url("https://raw.githubusercontent.com/csanry/torus/main/pipelines/kfp_components/training/model_evaluation.yaml")
-deploy_op = kfp.components.load_component_from_url("https://raw.githubusercontent.com/csanry/torus/main/pipelines/kfp_components/prediction/predict.yaml")
-
-
-# Define the pipeline
+# -------------------
+# Pipeline definition
+# -------------------
 @dsl.pipeline(
-   name='testing pipeline', # to change 
-   description='NIL', # to change 
+   name=ML_PIPELINE_NAME, 
+   description='Train pipeline for credit card defaults', 
    pipeline_root=PIPELINE_ROOT
 )
-def xgboost_test_pipeline(
+def ccd_train_pipeline(
 ):
     ingest = ingest_op(
-        source_project_id="pacific-torus-347809",
+        source_project_id=PROJECT_ID,
         source_table_url="dwh_pacific_torus.credit_card_defaults",
-        destination_project_id="pacific-torus-347809",
-        destination_bucket="mle-dwh-torus",
+        destination_project_id=PROJECT_ID,
+        destination_bucket=BUCKET_NAME,
         destination_file="raw/new_ccd_test_02.csv",
         dataset_location="US",
         extract_job_config={},
@@ -53,13 +64,13 @@ def xgboost_test_pipeline(
 
     tfdv_step = tfdv_op(
         input_data=basic_preprocessing.output, # basic_preprocessing.output,
-        output_path="gs://mle-dwh-torus/tfdv_expers/eval/evaltest.pb",
+        output_path=f"gs://{BUCKET_NAME}/tfdv_expers/eval/evaltest.pb",
         job_name='test-1',
         use_dataflow="False",
-        project_id="pacific-torus-347809",
-        region="asia-southeast1-c", # us-central1
-        gcs_temp_location='gs://mle-dwh-torus/tfdv_expers/tmp',
-        gcs_staging_location='gs://mle-dwh-torus/tfdv_expers',
+        project_id=PROJECT_ID,
+        region=REGION, # asia-southeast1
+        gcs_temp_location=f"gs://{BUCKET_NAME}/tfdv_expers/tmp",
+        gcs_staging_location=f"gs://{BUCKET_NAME}/tfdv_expers",
         whl_location="tensorflow_data_validation-0.26.0-cp37-cp37m-manylinux2010_x86_64.whl"
     ).after(basic_preprocessing)
 
@@ -92,20 +103,20 @@ def xgboost_test_pipeline(
         with dsl.Condition(model_eval.outputs['deploy'] == "True"):
             deploy = deploy_op(
                 # model_input_file = model_eval.outputs['evaluated_model'],
-                model_input_file = 	'gs://mle-dwh-torus/models/',
-                serving_container_image_uri = "us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.1-0:latest",
-                project_id= 'pacific-torus-347809',
-                region = 'us-central1'
+                model_input_file=f"gs://{BUCKET_NAME}/models/",
+                serving_container_image_uri="us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.1-0:latest",
+                project_id=PROJECT_ID,
+                region='REGION'
             )
             
-    with dsl.Condition(tfdv_drift.outputs['drift']=='false'):
+    with dsl.Condition(tfdv_drift.outputs["drift"]=="false"):
         deploy = deploy_op(
-                    # model_input_file = model_eval.outputs['evaluated_model'],
-                    model_input_file = 	'gs://mle-dwh-torus/models/',
-                    serving_container_image_uri = "us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.1-0:latest",
-                    project_id= 'pacific-torus-347809',
-                    region = 'us-central1'
-                )
+            # model_input_file = model_eval.outputs['evaluated_model'],
+            model_input_file=f"gs://{BUCKET_NAME}/models/",
+            serving_container_image_uri="us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.1-0:latest",
+            project_id=PROJECT_ID,
+            region=REGION
+            )
 
 if __name__ == "__main__": 
 
@@ -113,7 +124,7 @@ if __name__ == "__main__":
     from datetime import datetime 
 
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../pacific-torus.json"
-    id = datetime.now().strftime(f"%Y%m%d-%H%M")
+    ID = datetime.now().strftime(f"%Y%m%d-%H%M")
 
 
     # ------------------------------------
@@ -121,25 +132,25 @@ if __name__ == "__main__":
     # ------------------------------------
 
     compiler.Compiler().compile(
-        pipeline_func=xgboost_test_pipeline,
-        pipeline_name=f"ccd-train-pipeline-{id}",
-        package_path="./ccd-train-pipeline.json",
+        pipeline_func=ccd_train_pipeline,
+        pipeline_name=f"{ML_PIPELINE_NAME}-{ID}",
+        package_path=f"{ML_PIPELINE_NAME}.json",
         type_check=True,
     )
 
     # ----------------------------------
-    # Schedule and run a pipeline job 
+    # Schedule and run a pipeline job
     # ----------------------------------
     
     job = aip.PipelineJob(
-        display_name=f"ccd-train-pipeline-{id}",
-        template_path="./ccd-train-pipeline.json",
+        display_name=f"{ML_PIPELINE_NAME}-{ID}",
+        template_path=f"{ML_PIPELINE_NAME}.json",
         enable_caching=True,
-        job_id=f'ccd-train-pipeline-job-{id}',
+        job_id=f"{ML_PIPELINE_NAME}-job-{ID}",
         pipeline_root=PIPELINE_ROOT
     )
 
     job.run()
 
     # remove files
-    subprocess.call(["rm", "-r", "ccd-train-pipeline.json"])
+    subprocess.call(["rm", "-r", f"{ML_PIPELINE_NAME}.json"])
